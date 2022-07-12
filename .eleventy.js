01ad9635f8b566522b699ab4a3a266effe684ac7
andystevenson <andystevenson@mac.com>
2022-07-12 22:14:01 +0100

    first integration with netlify

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,7 @@
 const config = require('@andystevenson/11ty')
-const EleventyVitePlugin = require('@11ty/eleventy-plugin-vite')
-const { EleventyEdgePlugin } = require('@11ty/eleventy')
 
 module.exports = function (eleventyConfig) {
   const newConfig = config(eleventyConfig)
-
-  eleventyConfig.addPlugin(EleventyVitePlugin)
-  eleventyConfig.addPlugin(EleventyEdgePlugin)
 
   eleventyConfig.addWatchTarget('./public')
   eleventyConfig.addFilter('decimal', function (num, length) {
